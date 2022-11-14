47e9274ff9ce2ff67bef96971f0443bfd0e26b1b
Yamanadm <adsamu44@gmail.com>
2022-11-14 16:32:35 +0300

    detail completed

--- src/pages/detail/Detail.jsx
+++ src/pages/detail/Detail.jsx
@@ -1,11 +1,69 @@
-import React from 'react'
+import React from "react";
+import { useLocation } from "react-router-dom";
+import {
+  DetailContainer,
+  DetailPart,
+  HeaderContainer,
+  ImgContainer,
+  IngContainer,
+  OtherPart,
+} from "./Detail.style";
+import diet from "../../assets/diet.svg";
 
 const Detail = () => {
+  const { state } = useLocation();
   return (
-    <div>
-      Detail
-    </div>
-  )
-}
+    <DetailContainer>
+      <HeaderContainer>
+        <h1>{state.label}</h1>
+        <img src={diet} />
+      </HeaderContainer>
+      <DetailPart>
+        <OtherPart>
+          <h3>NUTRIENTS</h3>
+          <p>
+            {state.totalNutrients.CA.label} :{" "}
+            {Math.round(state.totalNutrients.CA.quantity)}
+            {state.totalNutrients.CA.unit}
+          </p>
+          <p>
+            {state.totalNutrients.CHOCDF.label} :{" "}
+            {Math.round(state.totalNutrients.CHOCDF.quantity)}
+            {state.totalNutrients.CHOCDF.unit}
+          </p>
+          <p>
+            {state.totalNutrients.CHOLE.label} :{" "}
+            {Math.round(state.totalNutrients.CHOLE.quantity)}
+            {state.totalNutrients.CHOLE.unit}
+          </p>
+          <p>
+            {state.totalNutrients.ENERC_KCAL.label} :{" "}
+            {Math.round(state.totalNutrients.ENERC_KCAL.quantity)}
+            {state.totalNutrients.ENERC_KCAL.unit}
+          </p>
+          <p>{state.totalWeight}</p>
+          <p>Calories: {Math.round(state.calories)}</p>
+          {state.digest.slice(0, 4).map((item, index) => (
+            <p key={index}>
+              {item.label} : {Math.round(item.total)}
+            </p>
+          ))}
+        </OtherPart>
+        <ImgContainer>
+          <img src={state.image} alt="img" />
+        </ImgContainer>
+        <IngContainer>
+          {state.ingredientLines.map((ingredient, index) => (
+            <div>
+              <p>
+                {index + 1}-) {ingredient}
+              </p>
+            </div>
+          ))}
+        </IngContainer>
+      </DetailPart>
+    </DetailContainer>
+  );
+};
 
-export default Detail
+export default Detail;
